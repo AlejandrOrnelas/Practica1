a18c6c50067c3b0fa0ba1dc9adc4af2ba43f49dc
husso <husso@LAPTOP-81EJPQ96>
2022-09-02 14:05:27 -0700

    Agregar archivos de proyecto.

---
+++ proyecto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alejandro Ornelas Montoya");
+            Console.ReadKey();
+            Console.WriteLine("25 Años");
+            Console.ReadKey();
+            Console.WriteLine("Mexicali,Baja California");
+            Console.ReadKey();
+        }
+    }
+}
